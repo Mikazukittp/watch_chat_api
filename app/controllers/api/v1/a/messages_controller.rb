a9b9c3f42f0ec46ca939952864cb9444ad36a933
matsumatsu20 <matsumatsu20@gmail.com>
2015-07-13 00:04:38 +0900

    android用のメッセージ疎通用の記述を直した

--- app/controllers/api/v1/a/messages_controller.rb
+++ app/controllers/api/v1/a/messages_controller.rb
@@ -7,8 +7,13 @@ class Api::V1::A::MessagesController < ApplicationController
   end
 
   def create
-    data = { message: "hi mom!" }
-    send_push(params[:registration_ids], data)
+
+    data = { message: "やっほー松田翔太だよー" }
+    send_push(params.require(:registration_ids), data)
+
+    @hogehoge = { :result => "プッシュ通知飛んだかな？"}
+    render :json => @hogehoge, status: :ok
+
   end
 
   private
@@ -16,9 +21,13 @@ class Api::V1::A::MessagesController < ApplicationController
   def send_push(registration_ids, data)
     n = Rpush::Gcm::Notification.new
     n.app = Rpush::Gcm::App.find_by_name("android_app")  # Rpush::GCM::Appインスタンスを設定
-    n.registration_ids = ["token", "..."] # registration_idを設定
-    n.data = { message: "hi mom!" } # カスタムデータ
+    n.registration_ids = [registration_ids] # registration_idを設定
+    n.data = { message: "やっほー松田翔太だよー" } # カスタムデータ
     n.save!
+  end
+
+  def user_params
+      params.permit(:registration_ids)
   end
 
 end
