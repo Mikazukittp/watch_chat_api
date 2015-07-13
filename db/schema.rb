89a9ed880946c2dc501d4b970c1367880865012e
matsumatsu20 <matsumatsu20@gmail.com>
2015-07-13 23:34:52 +0900

    プッシュサーバの情報を更新した

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710094000) do
+ActiveRecord::Schema.define(version: 20150713140308) do
+
+  create_table "messages", force: true do |t|
+    t.integer  "sender_id"
+    t.integer  "opponent_id"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "rpush_apps", force: true do |t|
     t.string   "name",                                null: false
