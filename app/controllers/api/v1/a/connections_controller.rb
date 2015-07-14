de385d88710f55aa9ac53e7203fc930fb5bf2deb
Sarashiya Ryosuke <ryosuke.sarashiya@gmail.com>
2015-07-15 03:45:07 +0900

    コネクションのcreateを実装しました。

--- app/controllers/api/v1/a/connections_controller.rb
+++ app/controllers/api/v1/a/connections_controller.rb
@@ -4,7 +4,23 @@ class Api::V1::A::ConnectionsController < Api::V1::A::BaseController
   end
 
   def create
+    @sendUser = User.find(params[:id])
+    
+    receiveUserCandidate = User.where(relation_id: nil)
+    rand_num = rand(receiveUserCandidate.size) - 1
+    @receiveUser = receiveUserCandidate[rand_num]
+
+    ##@sendUserのrelation_idに、receiveUserのidを代入。@receiveUserのrelation_idにsendUserのidを代入
+    User.transaction do
+      @sendUser.update!(relation_id: @receiveUser.id)
+      @receiveUser.update!(relation_id: @sendUser.id)
+    end
+      render json: @receiveUser, status: :created
+
+    rescue => e
+      render_500(e)
   end
+
 
   def destroy
 
