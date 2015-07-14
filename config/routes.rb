ce21e137c65e0d241825a01ec24baab1cafdf7bd
matsumatsu20 <matsumatsu20@gmail.com>
2015-07-14 23:29:11 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
       namespace :a do
-        resource :device_token, only: [:update]
-        resource :message, only: [:index, :show, :create]
+        resource :messages, only: [:index, :show, :create]
       end
 
       namespace :i do
-        resource :device_token, only: [:update]
-        resource :message, only: [:index, :show, :create]
+        resource :messages, only: [:index, :show, :create]
       end
     end
   end
