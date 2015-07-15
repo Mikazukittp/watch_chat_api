40c246891af78b57e7d8defca2275290caa6c09f
matsumatsu20 <matsumatsu20@gmail.com>
2015-07-16 01:25:07 +0900

    USERを作る際にデバイス情報を持たせるようした

---
+++ app/controllers/api/v1/base_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::A::BaseController < ApplicationController
+
+  def send_push_a(registration_ids, data)
+    n = Rpush::Gcm::Notification.new
+    n.app = Rpush::Gcm::App.find_by_name("android_app")  # Rpush::GCM::Appインスタンスを設定
+    n.registration_ids = [registration_ids] # registration_idを設定
+    n.data = data # カスタムデータ
+    n.save!
+  end
+
+  def send_push_i(device_token, alert)
+    n = Rpush::Apns::Notification.new
+    n.app = Rpush::Apns::App.find_by_name("ios_app") # Rpush::Apns::Appインスタンスを設定
+    n.device_token = device_token # デバイストークン
+    n.alert = alert  # プッシュメッセージ
+    n.data = { category: :remotePush } # カスタムデータ
+    n.save!
+  end
+
+end
