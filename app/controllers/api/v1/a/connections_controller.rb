cee98723199a21f69cc7339af5fe94077f8ea888
matsumatsu20 <matsumatsu20@gmail.com>
2015-07-15 22:25:50 +0900

    微修正

--- app/controllers/api/v1/a/connections_controller.rb
+++ app/controllers/api/v1/a/connections_controller.rb
@@ -6,11 +6,12 @@ class Api::V1::A::ConnectionsController < Api::V1::A::BaseController
   def create
     @sendUser = User.find(params[:id])
     if @sendUser.relation_id.present?
-      render_500({message: 'You have already connected to user(id: #{@sendUser.relation_id})'})
+      return render json: {message: "You have already connected to user(id: #{@sendUser.relation_id})"}, status: 500
     end
-    @receiveUser = User.where(relation_id: nil).sample
-    if @receiveuser.blank?
-      render_404({message: 'No conectable user.'})
+
+    @receiveUser = User.where.not(id: params[:id]).where(relation_id: nil).sample
+    if @receiveUser.blank?
+      return render json: {message: "No conectable user."}, status: 404
     end
 
     ##@sendUserのrelation_idに、receiveUserのidを代入。@receiveUserのrelation_idにsendUserのidを代入
@@ -18,12 +19,12 @@ class Api::V1::A::ConnectionsController < Api::V1::A::BaseController
       @sendUser.update!(relation_id: @receiveUser.id)
       @receiveUser.update!(relation_id: @sendUser.id)
     end
-      render json: @receiveUser, status: :created
 
-    rescue => e
-      render_500(e)
+    render json: @receiveUser, status: :created
+
+  rescue => e
+    render_500(e)
   end
-
 
   def destroy
 
@@ -34,8 +35,9 @@ class Api::V1::A::ConnectionsController < Api::V1::A::BaseController
       @user.update!(relation_id: nil)
       @opponent.update!(relation_id: nil)
     end
-      render json: @opponent, status: :ok
+    render json: @opponent, status: :ok
 
     rescue => e
-      render_500(e)
+    render_500(e)
   end
+end
